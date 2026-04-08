fa77c8cd26973eca7ff7177b319815f0b68224d2
Scalabrini17 <scalabriniexecutivo@gmail.com>
2026-04-08 15:49:28 -0300

    first commit Python

---
+++ Ativ01.py
@@ -0,0 +1,26 @@
+#PRATICA DA FUNÇÃO print
+
+#1
+print('Python na Escola de Programação da Alura')
+
+#2
+nome = input('Qual o seu nome: ')
+idade = int(input('Qual é a sua idade: '))
+print(f'Meu nome é {nome} e eu tenho {idade} anos ')
+
+#3
+print('''
+A
+L
+U
+R
+A''')
+
+#4
+pi_arredondado = 3.14
+print(f'O valor arredondado de pi é: {pi_arredondado}')
+
+'''
+# Uma forma alternativa para o format é a abordagem de .format()
+print('O valor arredondado de pi é: {:.2f}'.format(pi))
+'''
---
+++ app.py
@@ -0,0 +1,26 @@
+print('''     
+░█▀▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ 　 ░█▀▀▀ █─█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ 
+─▀▀▀▄▄ █▄▄█ █▀▀▄ █──█ █▄▄▀ 　 ░█▀▀▀ ▄▀▄ █──█ █▄▄▀ █▀▀ ▀▀█ ▀▀█ 
+░█▄▄▄█ ▀──▀ ▀▀▀─ ▀▀▀▀ ▀─▀▀ 　 ░█▄▄▄ ▀─▀ █▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀ ▀▀▀
+      ''') # sempre que quiser também pular uma linha ou formatação para o print pode se usar a aspas triplas ''' ''' (vou colocar um exemplo de formatação nbo final nas anotações)
+#i \n pula uma linha no terminal
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. ativar restaurante')
+print('4. sair \n') 
+
+opcao_escolha = input('Escolha uma opção: ')
+print(f'Você escolheu a opção: {opcao_escolha}')
+
+
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------------------------
+'''
+Anotações
+
+
+
+
+'''
