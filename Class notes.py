6b5e73aec07a49d0825c33de32da776bcca39685
Scalabrini17 <scalabriniexecutivo@gmail.com>
2026-04-08 17:06:36 -0300

    Adding new features, implementing functions, and updating class notes.

--- Class notes.py
+++ Class notes.py
@@ -2,5 +2,25 @@
 
 Anotações aqui!
 
+Usando no codigo esse script ele irá retornar que tipo de classe está retornando, 
+por exemplo a minha variavel opscao_escolhida retorna no terminal <class 'int'> indicando que o retorno é um número inteiro! 
+print(type(*variavel*))
+
+#aqui estou DEFININDO uma FUNÇÃO para o item 4
+def finalizar_app():
+    print('Encerrando o App...')
+    time.sleep(3)
+    os.system('cls')
+    print('App Finalizado com sucesso!')
+    time.sleep(2)
+    os.system('cls')
+
+O que é uma função?
+Função é um bloco de código que vai realizar 1 determinada ação, pode ser utilizada não só um a vez mas por varias vezes, inclusive
+se uma ação for feita em diferentes partes do seu código e irá se repetir é sempre bom criar uma função porque aí quando você quiser que seja
+executado essa função é só colocar o nome dela como por exemplo:
+
+else:
+    finalizar_app()
 
 '''
--- app.py
+++ app.py
@@ -1,26 +1,58 @@
-print('''     
+import os
+import time
+
+def exibir_nome_do_programa():
+      print('''    
+       
 ░█▀▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ 　 ░█▀▀▀ █─█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ 
 ─▀▀▀▄▄ █▄▄█ █▀▀▄ █──█ █▄▄▀ 　 ░█▀▀▀ ▄▀▄ █──█ █▄▄▀ █▀▀ ▀▀█ ▀▀█ 
 ░█▄▄▄█ ▀──▀ ▀▀▀─ ▀▀▀▀ ▀─▀▀ 　 ░█▄▄▄ ▀─▀ █▀▀▀ ▀─▀▀ ▀▀▀ ▀▀▀ ▀▀▀
+      
       ''') # sempre que quiser também pular uma linha ou formatação para o print pode se usar a aspas triplas ''' ''' (vou colocar um exemplo de formatação nbo final nas anotações)
-#i \n pula uma linha no terminal
-print('1. Cadastrar restaurante')
-print('2. Listar restaurante')
-print('3. ativar restaurante')
-print('4. sair \n') 
 
-opcao_escolha = input('Escolha uma opção: ')
-print(f'Você escolheu a opção: {opcao_escolha}')
+def exibir_opcoes():
+      #i \n pula uma linha no terminal
+      print('1. Cadastrar restaurante')
+      print('2. Listar restaurante')
+      print('3. ativar restaurante')
+      print('4. sair') 
+      print('5 Reiniciar \n')
+
+def finalizar_app():
+    print('Encerrando o App...')
+    time.sleep(3)
+    os.system('cls')
+    print('App Finalizado com sucesso!')
+    time.sleep(2)
+    os.system('cls')
+    #os.system('python app.py') REINICIAR
+    #aqui estou DEFININDO uma FUNÇÃO para o item 4
+
+def reiniciar_app():
+   print('Reiniciando o App...')
+   time.sleep(3)
+   os.system('cls')
+   os.system('python app.py')
+
+def escolher_opcao():
+    opcao_escolha = int(input('Escolha uma opção: '))
+
+    if opcao_escolha == 1:
+      print('Cadastrar restaurante')
+    elif opcao_escolha == 2:
+      print('Listar restaurantes')
+    elif opcao_escolha == 3:
+      print('Ativar restaurantes')
+    elif opcao_escolha == 4:
+      finalizar_app()
+    else:
+      reiniciar_app()  
+
+def main():
+    exibir_nome_do_programa()
+    exibir_opcoes()
+    escolher_opcao()
 
 
-
-
-
-#--------------------------------------------------------------------------------------------------------------------------------------------
-'''
-Anotações
-
-
-
-
-'''
+if __name__ == '__main__':
+    main()
