a05634be61ef1f7a3d785b51795a91df68209362
Scalabrini17 <itallomartins2007@gmail.com>
2026-04-24 14:54:09 -0300

    organizing folders

--- level-1_introduction/app.py
+++ level-1_introduction/app.py
@@ -8,6 +8,7 @@ restaurantes = [{"nome":"Praça", "categoria":"Japonesa", "ativo":False},
                 {"nome":"Cantina", "categoria":"Italiana", "ativo":False}]
 
 def exibir_nome_do_programa():
+      ''' Essa função é chamada todas as vezes que o menu é chamado, ela é o título do app '''
       print('''    
        
 ░█▀▀▀█ █▀▀█ █▀▀▄ █▀▀█ █▀▀█ 　 ░█▀▀▀ █─█ █▀▀█ █▀▀█ █▀▀ █▀▀ █▀▀ 
@@ -17,6 +18,7 @@ def exibir_nome_do_programa():
       ''') # sempre que quiser também pular uma linha ou formatação para o print pode se usar a aspas triplas ''' ''' (vou colocar um exemplo de formatação nbo final nas anotações)
 
 def exibir_opcoes():
+      ''' Essa função é para exibir as opções de escolha do usuário '''
       #i \n pula uma linha no terminal
       print('1. Cadastrar restaurante')
       print('2. Listar restaurante')
@@ -25,6 +27,7 @@ def exibir_opcoes():
       print('5. Reiniciar \n')
 
 def finalizar_app():
+    ''' Essa função serve para a finalização do código, ou do app no caso. Ele termina o nosso código '''
     print('Encerrando o App...')
     time.sleep(2)
     exibir_subtitulo('App Finalizado com sucesso!')
@@ -33,6 +36,7 @@ def finalizar_app():
     #aqui estou DEFININDO uma FUNÇÃO para o item 4
 
 def reiniciar_app():
+   ''' Essa função é para reiniciar o app, caso queria reiniciar tem essa escolha e função '''
    print('Reiniciando o App...')
    time.sleep(2)
    os.system('clear')
@@ -40,16 +44,19 @@ def reiniciar_app():
    # Para o app de windows modificar para ('python app.py')
 
 def opcao_invalida():
+   ''' Essa função é para quando tiver alguma opação invalida tanto quando colocar nas opções de inicio quanto nas outras abas do app '''
    print('Essa opção que você digitou é invalida...\n')
    voltar_ao_menu()
   
 def voltar_ao_menu():
+   ''' Essa função serve para voltar ao menu, quando terminar de cadastrar por exemplo o restaurante essa função deve ser chamada para que a pessoa retorne ao inicio '''
    input('\nDigite uma tecla qualquer para retornar ao menu principal!')
    print('Carregando...')
    time.sleep(2)
    main()
 
 def exibir_subtitulo(texto):
+   ''' Essa função serve para Exibir o subtitulo, geralmente chamada em toda aba assim que é chamada a escolha das opções '''
    linha = '*' * (len(texto))
    os.system('clear')
    print(linha)
@@ -58,6 +65,7 @@ def exibir_subtitulo(texto):
    print() 
 
 def cadastrar_novo_restaurante():
+   ''' Essa função é a de cadastros de restaurante, geralmente é chamada quando escolhida nas opções iniciais e ela faz o cadastro de um novo restaurante na lista, com todos os seus atributos do dicionario  '''
    exibir_subtitulo('Cadastro de novos restaurantes')
    nome_do_restaurante = input('Digite o nome do restaurante que deseja cadastrar: ')
    categoria = input(f'Digite a categoria do seu restaurante {nome_do_restaurante}: ')
@@ -67,6 +75,7 @@ def cadastrar_novo_restaurante():
    voltar_ao_menu()
 
 def listar_restaurantes(): 
+   ''' Essa funçã serve para listar todos os restaurante cadastrados na lista de dicionarios, e aparece também seus atributos  '''
    exibir_subtitulo('Listagem de Restaurantes')
 
    print(f'{'Nome do restaurante'.ljust(22)} | {'Categoria do restaurante'.ljust(22)} | {'Status'}')
@@ -81,6 +90,7 @@ def listar_restaurantes():
    voltar_ao_menu()
 
 def alternar_estado_restaurante():
+   ''' Essa função serve para alterar o estado do restaurante se ele estiver ativo então quando escolhido ele desativa, se desativado quando escolhido ele ativa  '''
    exibir_subtitulo('Alternando o estado do restaurante')
    
    nome_restaurante = input('Digite o nome do restaurante que deseja alternar o estado: ')
@@ -100,6 +110,7 @@ def alternar_estado_restaurante():
 
 
 def escolher_opcao():
+    ''' Essa função serve para escolher uma das opções, quando esclhido a opção o programa puxa uma das outras funções ou "Abas" para funcionar o app '''
     try:
       opcao_escolha = int(input('Escolha uma opção: '))
 
@@ -126,6 +137,7 @@ def escolher_opcao():
       opcao_invalida() 
 
 def main():
+    ''' Essa função serve para iniciar ou sempre que o menu é chamado, em alguns caso pode ser chamado no meio do programa em outros pode ser chamado sempre altomaticamente no inicio do programa, ele sempre irá chamar funções principais, como o titúlo, exibir as opções e escolher a opção. '''
     os.system('clear')
     # para o app no windows modificar para ('cls')
     exibir_nome_do_programa()
